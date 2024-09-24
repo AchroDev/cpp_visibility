c338a32c312a1747d48170a8e19414ff8aa01f05
AchroDev <achrodev@proton.me>
2024-09-23 22:28:00 -0500

    Added string include and example methods

--- main.cpp
+++ main.cpp
@@ -1,4 +1,15 @@
 #include <iostream>
+#include <string>
+
+// Visibility doesn't affect performance, it mainly helps with code readability
+
+class Entity
+{
+};
+
+class Player : public Entity
+{
+};
 
 int main()
 {
