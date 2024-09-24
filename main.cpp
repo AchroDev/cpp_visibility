45d5d29c830ff7d44dc12fd8e599c264591c131e
AchroDev <achrodev@proton.me>
2024-09-23 22:38:48 -0500

    Added further private examples

--- main.cpp
+++ main.cpp
@@ -10,19 +10,31 @@ class Entity
     // Private means only this class and a "friend" class/function can access the data
 private:
     int X, Y;
+    void Print() {} // example method to further show private usage
 
 public:
-    Entity() { X = 0; }
+    Entity()
+    {
+        X = 0;
+        Print(); // Print can be used here as it is within the class scope
+    }
 };
 
 class Player : public Entity
 {
+public:
+    Player()
+    {
+        X = 2;   // Still can't access it here as Player is outside of the class scope
+        Print(); // Cannot use Print here as Player is outside of the class scope
+    }
 };
 
 int main()
 {
     Entity e;
-    e.X = 2; // Although e is an Entity, X is private and cannot be called outside of the class scope
+    e.Print(); // Cannot use Print here as 'e' is outside of the class scope
+    e.X = 2;   // Although e is an Entity, X is private and cannot be called outside of the class scope
 
     std::cin.get();
 }
