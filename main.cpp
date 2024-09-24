e811b8a546f9c1cda9fb7607fef66995641d68f1
AchroDev <achrodev@proton.me>
2024-09-23 22:34:14 -0500

    Added private example

--- main.cpp
+++ main.cpp
@@ -3,8 +3,16 @@
 
 // Visibility doesn't affect performance, it mainly helps with code readability
 
+// Remember a class is default private meaning anything outside of the class scope cannot access it
 class Entity
 {
+
+    // Private means only this class and a "friend" class/function can access the data
+private:
+    int X, Y;
+
+public:
+    Entity() { X = 0; }
 };
 
 class Player : public Entity
@@ -13,6 +21,8 @@ class Player : public Entity
 
 int main()
 {
+    Entity e;
+    e.X = 2; // Although e is an Entity, X is private and cannot be called outside of the class scope
 
     std::cin.get();
 }
